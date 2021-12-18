0a86639474f430653243c926c3d2c7168369e675
NicolaiBlood <nicolaiblood@gmail.com>
2021-12-17 20:41:20 -0500

    hello again

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<img src="images/vardy-leicester.jpeg" alt="Jamie Vardy">
-	<h1>{name}!</h1>
+	<h1> Hello {name}!</h1>
 	
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	
