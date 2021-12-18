aec965e3dd99e19d986a5dbffe0cf20b4bf1e4e1
NicolaiBlood <nicolaiblood@gmail.com>
2021-12-17 22:28:55 -0500

    Vardy => Button

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 	<h1> Hello {name}!</h1>
 	
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<img src="./images/vardy-leicester.jpeg" alt="Jamie Vardy">
+	<button>chill chill</button>
 </main>
 
 <style>
